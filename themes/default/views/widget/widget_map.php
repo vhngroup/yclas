c00a912364182bb71a6d63a6d4d4e7b4714e4ca8
Oliver <oliver@open-classifieds.com>
2018-08-19 10:49:36 -0500

    Fixes widget map

--- themes/default/views/widget/widget_map.php
+++ themes/default/views/widget/widget_map.php
@@ -7,8 +7,8 @@
 <?endif?>
 
 <div class="panel-body">
-    <iframe frameborder="0" noresize="noresize" 
-            height="<?=$widget->map_height+($widget->map_height*0.10)?>px" width="100%" 
-            src="<?=Route::url('map')?>?height=<?=$widget->map_height?>&controls=0&zoom=<?=$widget->map_zoom?><?=(Model_Category::current()->loaded())?'&category='.Model_Category::current()->seoname:''?><?=(Model_Location::current()->loaded())?'&location='.Model_Location::current()->seoname:''?>">
+    <iframe frameborder="0" noresize="noresize"
+            height="<?=intval($widget->map_height)+(intval($widget->map_height)*0.10)?>px" width="100%"
+            src="<?=Route::url('map')?>?height=<?=intval($widget->map_height)?>&controls=0&zoom=<?=$widget->map_zoom?><?=(Model_Category::current()->loaded())?'&category='.Model_Category::current()->seoname:''?><?=(Model_Location::current()->loaded())?'&location='.Model_Location::current()->seoname:''?>">
     </iframe>
 </div>
