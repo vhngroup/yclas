f9d9ff2b0230f733991697cf372de5ee47e29632
Oliver <oliver@open-classifieds.com>
2018-09-03 11:19:21 -0500

    Fixes map home iframe route

--- themes/default/views/pages/map/home.php
+++ themes/default/views/pages/map/home.php
@@ -1,8 +1,8 @@
 <iframe
     width="100%"
-    height="<?=(empty(core::config('advertisement.homepage_map_height'))?'400px':core::config('advertisement.homepage_map_height'))?>" 
+    height="<?=(empty(core::config('advertisement.homepage_map_height'))?'400px':core::config('advertisement.homepage_map_height'))?>"
     frameBorder="0"
-    src="/map.html"
+    src="<?= Route::url('map') ?>"
     class="homepage_map"
     <?=((core::config('advertisement.homepage_map_allowfullscreen')==1)?'allowfullscreen':'')?>
     >
