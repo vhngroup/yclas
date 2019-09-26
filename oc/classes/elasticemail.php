4af33653ce532438f9b1ca0410566b82762e0557
Oliver <oliver@open-classifieds.com>
2019-09-25 20:35:56 -0500

    Add reply to elasticemail

--- oc/classes/elasticemail.php
+++ oc/classes/elasticemail.php
@@ -17,12 +17,12 @@ class ElasticEmail {
      * Send Elastic Email using cURL (libcurl) in PHP
      *
      */
-    public static function send($to,$to_name='', $subject, $body_html, $from, $from_name) 
+    public static function send($to,$to_name='', $subject, $body_html, $reply, $replyName)
     {
-        
+
         // Initialize cURL
         $ch = curl_init();
-        
+
         // Set cURL options
         curl_setopt($ch, CURLOPT_URL, 'https://api.elasticemail.com/mailer/send');
         curl_setopt($ch, CURLOPT_POST, 1);
@@ -31,22 +31,24 @@ class ElasticEmail {
         if (is_array($to))
         {
             $to_aux = '';
-            foreach ($to as $contact) 
+            foreach ($to as $contact)
                  $to_aux .= $contact['name'].' <'.$contact['email'].'>;';
 
              $to = $to_aux;
         }
         elseif($to_name!='')
             $to = $to_name . ' <'.$to.'>;';
-        
+
         $data = 'username='.urlencode(Core::config('email.elastic_username')).
                 '&api_key='.urlencode(Core::config('email.elastic_username')).
-                '&from='.urlencode($from).
-                '&from_name='.urlencode($from_name).
+                '&reply_to='.urlencode($reply).
+                '&reply_to_name='.urlencode($replyName).
+                '&from='.urlencode(core::config('email.notify_email')).
+                '&from_name='.urlencode(core::config('email.notify_name')).
                 '&to='.urlencode($to).
                 '&subject='.urlencode($subject).
                 '&body_html='.urlencode($body_html);
-        
+
         // Set parameter data to POST fields
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
@@ -59,28 +61,28 @@ class ElasticEmail {
 
         //timeout
         curl_setopt($ch,CURLOPT_TIMEOUT, 2);
-        
+
         // Set to receive server response
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        
+
         // Set cURL to verify SSL
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        
+
         // Get result
         $result = curl_exec($ch);
-        
+
         // Close cURL
         curl_close($ch);
-        
+
         return ($result === false) ? FALSE : TRUE;
     }
 
     /**
      * subscribes a user to a list
      * @param  string $listname
-     * @param  string $email  
+     * @param  string $email
      * @param  string $name
-     * @return json   
+     * @return json
      */
     public static function subscribe($listname, $email, $name)
     {
@@ -96,8 +98,8 @@ class ElasticEmail {
     /**
      * unsubscribes a user from a list
      * @param  string $listname
-     * @param  string $email  
-     * @return json   
+     * @param  string $email
+     * @return json
      */
     public static function unsubscribe($listname, $email)
     {
